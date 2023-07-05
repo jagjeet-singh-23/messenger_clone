690672f2395aed6a0fa2d25dbf36f78d1e512f84
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-07-05 15:39:33 +0530

    new file:   app/api/settings/route.ts

---
+++ app/api/settings/route.ts
@@ -0,0 +1,35 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+import prisma from "@/app/libs/prismadb";
+
+export async function POST(
+    request: Request
+) {
+    try {
+        const currentUser = await getCurrentUser();
+        const body = await request.json();
+        const {
+            name,
+            image
+        } = body;
+
+        if (!currentUser?.id) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const updatedUser = await prisma.user.update({
+            where: {
+                id: currentUser.id
+            },
+            data: {
+                image,
+                name,
+            }
+        });
+
+        return NextResponse.json(updatedUser);
+    } catch (error) {
+        console.log("SETTINGS_ERROR", error);
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
+}
