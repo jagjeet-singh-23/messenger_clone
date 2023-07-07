e19bb5c6f4db1e2ea9c301da0b2f4fcb5585eccc
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-07-07 18:41:29 +0530

    modified:   app/api/messages/route.ts 	modified:   app/conversations/[conversationId]/components/Body.tsx 	new file:   app/libs/pusher.ts 	modified:   package-lock.json 	modified:   package.json

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import prisma from "@/app/libs/prismadb"
+import { pusherServer } from "@/app/libs/pusher";
 
 export async function POST(
     request: Request,
@@ -66,8 +67,17 @@ export async function POST(
             }
         });
 
-        return NextResponse.json(newMessage);
+        await pusherServer.trigger(conversationId, 'messages:new', newMessage);
+        const lastMessage = updatedConversation.messages[updatedConversation.messages.length - 1];
 
+        updatedConversation.users.map((user) => {
+            pusherServer.trigger(user.email!, 'conversation:update', {
+                id: conversationId,
+                messages: [lastMessage]
+            });
+        })
+
+        return NextResponse.json(newMessage);
     } catch (error: any) {
         console.log(error, "MessageError");
         return new NextResponse('Internal Server Error', { status: 500 });
