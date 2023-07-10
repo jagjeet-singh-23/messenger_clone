b65acc9f12d95d73fcee81ddb0e89a02b8e9bfab
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-07-10 14:35:16 +0530

    modified:   app/libs/pusher.ts 	new file:   pages/api/pusher/auth.ts

---
+++ pages/api/pusher/auth.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import { pusherServer } from "@/app/libs/pusher";
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  const session = await getServerSession(request, response, authOptions);
+  if (!session?.user?.email) {
+    return response.status(401);
+  }
+
+  const socket_id = request.body.socket_id;
+  const channel = request.body.channel_name;
+  const data = {
+    user_id: session.user.email,
+  };
+  const authResponse = pusherServer.authorizeChannel(socket_id, channel, data);
+  return response.send(authResponse);
+}
